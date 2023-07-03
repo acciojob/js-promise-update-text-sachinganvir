2e5223b04fb58ab719a8800373be3f6d62b3f78a
sachinganvir <120235946+sachinganvir@users.noreply.github.com>
2023-07-03 15:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// sa
+//your JS code here. If required.
+function updateElementText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Update HTML element text
+const outputElement = document.getElementById("output");
+updateElementText().then((text) => {
+  outputElement.textContent = text;
+});
+// se
